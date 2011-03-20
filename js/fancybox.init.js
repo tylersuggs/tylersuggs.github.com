a6c3def16022a6b6f86e7a1297d8a310f04d89c5
Ty Suggs <git@tysug.gs>
2011-03-20 02:36:18 -0400

    extracted fancybox init code to separate file.

---
+++ js/fancybox.init.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('.fancyquote').fancybox({
+		'width': '75%',
+		'height': '75%',
+		'autoScale': false,
+		'autoDimensions': false,
+		'titlePosition': 'inside'
+	});
+	
+	$('.fancyframe').fancybox({
+		'width': '75%',
+		'height': '75%',
+		'autoScale': false,
+		'autoDimensions': false,
+		'titlePosition': 'inside',
+		'type': 'iframe'
+	})	
+});
